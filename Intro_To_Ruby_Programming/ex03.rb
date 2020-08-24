eed590f3638419e129c1cb2f9e0854a7935eb6f4
Katie Bailey <kfslavin@gmail.com>
2020-08-24 09:29:56 -0400

    added third and fourth exercises

---
+++ Intro_To_Ruby_Programming/ex03.rb
@@ -0,0 +1,10 @@
+movies = {
+  The_Shawshank_Redemption: 1994,
+  The_Godfather: 1972,
+  The_Dark_Knight: 2008, 
+  Twelve_Angry_Men: 1957,
+  Hamilton: 2020,
+  Inception: 2010
+}
+
+movies.each {|key, value| puts value}
---
+++ Intro_To_Ruby_Programming/ex04.rb
@@ -0,0 +1,9 @@
+movie_dates = [1994, 1972, 2008, 1957, 2020, 2010]
+
+puts movie_dates[0]
+puts movie_dates[1]
+puts movie_dates[2]
+puts movie_dates[3]
+puts movie_dates[4]
+puts movie_dates[5]
+
