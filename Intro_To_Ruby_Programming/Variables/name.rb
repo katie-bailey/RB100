51b06c0cb183f0a06ba2871365169507671c789d
Katie Bailey <kfslavin@gmail.com>
2020-08-24 10:53:08 -0400

    adding exercises two, three and four

--- Intro_To_Ruby_Programming/Variables/name.rb
+++ Intro_To_Ruby_Programming/Variables/name.rb
@@ -1,3 +1,10 @@
-puts "What is your name?"
-name = gets.chomp
-puts "Hello, #{name}!"
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+puts "Hello, #{first_name } #{last_name}!"
+
+10.times {puts first_name}
+
+
+
