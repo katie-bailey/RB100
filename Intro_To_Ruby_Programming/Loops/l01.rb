7262e0dcad9d4bc803d4ba8c6a77a94391ed30cd
Katie Bailey <kfslavin@gmail.com>
2020-08-25 08:48:06 -0400

    added the first exercise

---
+++ Intro_To_Ruby_Programming/Loops/l01.rb
@@ -0,0 +1,6 @@
+puts "For this exercise the each method goes over each
+element in the array and then continues on to the next
+element. The final solution will be [1, 2, 3, 4, 5] 
+because the return value of each will always be the
+starting array."
+
