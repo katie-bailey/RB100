865cae54c7b50d18a4c393b2ad6ffe357e2b03cc
Katie Bailey <kfslavin@gmail.com>
2020-08-25 09:01:50 -0400

    added second exercise

---
+++ Intro_To_Ruby_Programming/Loops/l02.rb
@@ -0,0 +1,9 @@
+puts "Would you like to continue?"
+answer = gets.chomp
+
+while answer != "STOP"
+  puts "Ok, we will continue."
+  puts "Would you like to continue?"
+answer = gets.chomp
+end
+puts "Ok, we will stop now."
