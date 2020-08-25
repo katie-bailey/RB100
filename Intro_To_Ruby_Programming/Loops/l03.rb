14da1e075b0caeb26364b5d5bb789da7598002b8
Katie Bailey <kfslavin@gmail.com>
2020-08-25 09:10:53 -0400

    added third exercise

---
+++ Intro_To_Ruby_Programming/Loops/l03.rb
@@ -0,0 +1,19 @@
+def division(num)
+  puts num
+    if num > 0
+      division(num / 2)
+    end
+end
+
+division(4444)
+
+def subtraction(num)
+  puts num
+    if num > 0
+      subtraction(num - 1)
+    end
+end
+
+subtraction(42)
+
+
